5909b82eec02dd750aa1cdd986373ed1794bf568
Benjamin Cellini <31375763+boc5244@users.noreply.github.com>
2021-09-28 17:38:35 -0400

    Update abdomen tracker of moth option

--- abdomen_tracker/batch_abdomen_tip.m
+++ abdomen_tracker/batch_abdomen_tip.m
@@ -1,19 +1,21 @@
-function [] = batch_abdomen_tip(root, vidvar, npts, mask_mode, loop_mask, playback, export)
+function [] = batch_abdomen_tip(root, vidvar, npts, mask_mode, loop_mask, playback, export, animal_class)
 %% batch_abdomen_tip: runs abdomen tracker for user selected video files
 %
 %   INPUT:
-%       root        :   root directory
-%       vidvar      :   video variable name
-%       npts        :   # of points for tip tracker
-%       mask_mode 	:   [x y]. x is a boolean that sets whether initial mask position is 
-%                       automatically computed (true) or not (false). y is a boolean that sets 
-%                       whether initial mask position is editable by user after being set by 
-%                       x (true) or left using the initial position (false). 
-%                       Ex: [1 1] (set automatically & and let user edit after)
-%                       Can also specifiy a mask structure saved from previous tracker.
-%       loop_mask   :   if true, then only set mask for 1st file
-%       playback    :   playback rate (show a frame in increments of "playback")
-%                       If false, then don't show anything (default = 1)
+%       root            :   root directory
+%       vidvar          :   video variable name
+%       npts            :   # of points for tip tracker
+%       mask_mode       :   [x y]. x is a boolean that sets whether initial mask position is 
+%                           automatically computed (true) or not (false). y is a boolean that sets 
+%                           whether initial mask position is editable by user after being set by 
+%                           x (true) or left using the initial position (false). 
+%                           Ex: [1 1] (set automatically & and let user edit after)
+%                           Can also specifiy a mask structure saved from previous tracker.
+%       loop_mask       :   if true, then only set mask for 1st file
+%       playback        :   playback rate (show a frame in increments of "playback")
+%                           If false, then don't show anything (default = 1)
+%       export          :   save the tracking video
+%       animal_class  	:   (1 = fly, 2 = moth)
 %
 %   OUTPUT:
 %       -
@@ -26,7 +28,7 @@ function [] = batch_abdomen_tip(root, vidvar, npts, mask_mode, loop_mask, playba
 % loop_mask = true;
 % playback = 20;
 
-[FILES, PATH] = uigetfile({'*.mat', 'MAT-files'},'Select videos', root, 'MultiSelect','on');
+[FILES, PATH] = uigetfile({'*.mat'},'Select videos', root, 'MultiSelect','on');
 FILES = string(FILES);
 nfile = length(FILES);
 
@@ -38,8 +40,7 @@ for n = 1:nfile
     disp('---------------------------------------')
     
     % Load data
-    Data = load(fullfile(PATH,FILES(n)));
-    t_v = Data.t_v;
+    Data = load(fullfile(PATH,FILES(n)), vidvar);
     vid = Data.(vidvar);
     
     if export
@@ -50,9 +51,9 @@ for n = 1:nfile
     end
 
     % Run tracker
-    [abdomen_data, abdomen_mask] = track_abdomen_vid(vid, abdomen_mask, npts, playback, vidpath);
+    [abdomen_data, abdomen_mask] = track_abdomen_vid(vid, abdomen_mask, npts, playback, vidpath, animal_class);
     
- 	save(fullfile(abdomen_dir,FILES{n}),'-v7.3','abdomen_data', 'abdomen_mask', 't_v')
+ 	save(fullfile(abdomen_dir,FILES{n}),'-v7.3','abdomen_data', 'abdomen_mask')
     
     if loop_mask
         % use last mask for next file
